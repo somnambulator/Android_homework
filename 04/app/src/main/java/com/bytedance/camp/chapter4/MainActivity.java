767624bcce3e444dc5c97e8d7eeefda7646dcf2e
somnambulator <yuan_lee1@163.com>
2021-07-15 14:34:30 +0800

    first commit

---
+++ 04/app/src/main/java/com/bytedance/camp/chapter4/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bytedance.camp.chapter4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bytedance.camp.chapter4.widget.ClockView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
