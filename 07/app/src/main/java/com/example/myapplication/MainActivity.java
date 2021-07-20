a65e2f55d6c99e028ea0c365701ff753e73e88dc
somnambulator <yuan_lee1@163.com>
2021-07-20 19:52:02 +0800

    add fullscreen

---
+++ 07/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnPic = findViewById(R.id.btn_pic);
+        Button btnVideo = findViewById(R.id.btn_video);
+
+        btnPic.setOnClickListener(v -> {
+            Intent intent = new Intent(this, PictureDetailActivity.class);
+            startActivity(intent);
+        });
+        btnVideo.setOnClickListener(v -> {
+            Intent intent = new Intent(this, VideoDetailActivity.class);
+            startActivity(intent);
+        });
+    }
+}
